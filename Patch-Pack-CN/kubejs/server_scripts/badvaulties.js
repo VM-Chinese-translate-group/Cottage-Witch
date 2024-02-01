d2f124f37745b64e7cebcf20c12dcbd077b60295
nageih <zhwlang2008@outlook.com>
2024-02-01 20:41:41 +0800

    move

--- Patch-Pack-CN/kubejs/server_scripts/badvaulties.js
+++
@@ -1,6 +0,0 @@
-BlockEvents.rightClicked('create:item_vault', e => {
-    if(e.item.id == 'pipez:item_pipe')
-    e.cancel()
-    e.server.runCommandSilent(`tell @p 请不要将Pipez连接到物品保险库，这会引起严重的游戏延迟。`)
-})
-
