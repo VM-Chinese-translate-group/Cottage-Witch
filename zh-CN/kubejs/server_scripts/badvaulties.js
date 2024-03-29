b5e00134593926e83080fd6d2ce37675b170d785
nageih <zhwlang2008@outlook.com>
2024-03-29 21:08:53 +0800

    1.16.2

--- zh-CN/kubejs/server_scripts/badvaulties.js
+++ zh-CN/kubejs/server_scripts/badvaulties.js
@@ -1,6 +1,11 @@
-BlockEvents.rightClicked('create:item_vault', e => {
-    if(e.item.id == 'pipez:item_pipe')
-    e.cancel()
-    e.server.runCommandSilent(`tell @p 请不要将Pipez连接到物品保险库，这会引起严重的游戏延迟。`)
-})
 
+BlockEvents.rightClicked('block.right_click', event => {
+    //main hand only
+    const { block, hand, item, world, player } = event;
+    if (item.id == 'pipez:item_pipe' && block.id == 'create:item_vault') {
+        let cmd = `/tell ${player.username} 请不要将Pipez连接到物品保险库，这会引起严重的游戏延迟。`
+        console.log(cmd)
+        event.server.runCommand(cmd)
+        event.cancel()
+    }
+});
